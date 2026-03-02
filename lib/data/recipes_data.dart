cf264422751ea48463e78a97980bc736ac5e2379
Odinjor <odinjorud@gmail.com>
2026-03-02 17:51:08 -0500

    All recipes and recipe data was added

---
+++ lib/data/recipes_data.dart
@@ -0,0 +1,91 @@
+
+import '../models/recipe.dart';
+
+final List<Recipe> sampleRecipes = [
+
+  Recipe(
+    name:'Jollof Rice',
+    imagePath: 'assets/images/Jollof.jpg',
+    ingredients: [
+      'For the pepper stew base:\n',
+      ' - 2 red bell peppers, stems and seeds removed and roughly chopped\n',
+      ' - 1 medium plum tomato, roughly chopped\n',
+      ' - 1/2 medium red onion, roughly chopped\n',
+      ' - 1 scotch bonnet or habanero pepper, stem trimmed\n',
+      ' - 1-inch piece of fresh ginger, peeled\n',
+      ' - 3 cloves of garlic\n',
+
+      'For the jollof:\n',
+      ' - 1/2 cup vegetable oil or peanut oil\n',
+      ' - 1/2 medium red onion, sliced\n',
+      ' - 1/4 cup tomato paste\n',
+      ' - 2 cups of basmati or jasmine rice, rinsed well and drained\n',
+      ' - 1 1/2 teaspoons Jamaican-style curry powder\n',
+      ' - 1 chicken or beef bouillon cube or 1 teaspoon bouillon powder\n',
+      ' - 2 teaspoons dried thyme\n',
+      ' - 1/2 teaspoon garlic powder\n',
+      ' - 1/2 teaspoon onion powder\n',
+      ' - 1/8 teaspoon ground nutmeg\n',
+      ' - 2 dried bay leaves\n',
+      ' - 1 1/2 teaspoons kosher salt\n',
+      ' - 2 cups low-sodium chicken broth or water\n',
+      
+      'Optional garnishes:\n',
+      ' - 2 tablespoons unsalted butter, cut into 4 cubes\n',
+    ],
+    instructions: 'Preheat the oven to 350°F (175°C).\n Prepare the pepper stew base: Add the bell peppers, tomato, onion, scotch bonnet or habanero pepper, ginger, and garlic into a blender, and blend until smooth. Set aside.\n Fry the onions: Heat the oil in a large Dutch oven (mine holds 3 quarts) or a heavy-bottomed pot over medium heat. Add the onions and fry for 2 to 3 minutes, until fragrant and the edges get a little brown. Scoop out about a third of the fried onions on a small plate or bowl, and set them aside. You will use it to top the jollof rice at the end.\n Cook the tomato paste: Add the tomato paste and cook it for 4 to 5 minutes, stirring frequently. The oil should turn red, and the paste should look grainy. Reduce the heat if the paste starts sticking to the bottom of the pot and burning.\n Cook the pepper stew base: Carefully stir in pepper stew base, and cook it uncovered for 20 to 25 minutes until reduced by about half. Stir the mixture occasionally with a wooden spoon and scrape up any bits that stick to the bottom of the pot. The mixture will splatter quite a bit! Keep it covered with a splatter guard or place a lid on top slightly ajar so that moisture can escape. Adjust the heat as needed if you notice it burning or if it\\’s not simmering.\n Add the rice and seasonings: Stir in the rice, curry powder, bouillon cube, dried thyme, garlic powder, onion powder, nutmeg, bay leaves, and salt. Cook for about 3 minutes, stirring frequently. Then add the chicken stock, and bring it to a simmer.\n Bake the jollof rice: Turn the heat off and tightly cover the pot with foil and a lid. Transfer the pot to the preheated oven and bake, undisturbed, for about 40 minutes.\n Add butter: If using the butter (I highly recommend it!), remove the pot from the oven and dollop the pats of butter on top. Sprinkles the reserved fried onions on top too. Cover the pot again with the foil and lid, and bake for 5 minutes.\n Serve: Gently stir the rice to incorporate the butter, peas, and onions. Discard the bay leaves and transfer the rice to a platter, if you\\’d like. Serve warm. Store leftovers tightly covered in the refrigerator for up to 4 days. Reheat in the microwave in 1-minute intervals until heated through.  ',
+  ),
+
+  Recipe(
+    name: 'Suya',
+    imagePath: 'assets/images/Suya.jpg',
+    ingredients: [
+      '1 tablespoon finely ground roasted peanuts\n',
+      '1 teaspoon ground paprika\n',
+      '1 teaspoon salt\n',
+      '1/2 teaspoon ground ginger\n',
+      '1/2 teaspoon garlic powder\n',
+      '1/2 teaspoon onion powder\n',
+      '1 1/2 pounds beef tri-tip steak, cut into bite-size pieces\n',
+      '1/4 onion, cut into bite-size pieces\n',
+      '4 mushrooms, halved\n',
+    ],
+    instructions: 'Step 1: Mix ground peanuts, cayenne pepper, paprika, salt, ginger, garlic powder, and onion powder together in a bowl.\n Step 2: Combine spice mixture and beef in a resealable plastic bag; shake until well coated. Marinate in the refrigerator, about 30 minutes.\n Step 3: Preheat an outdoor grill for medium-high heat and lightly oil the grate.\n Step 4: Place beef, bell pepper, onion, and mushrooms in alternating order onto skewers.\n Step 5: Cook on the preheated grill, flipping once, until meat is browned and vegetables are soft, 10 to 15 minutes.',
+  ),
+
+  Recipe(
+    name: 'Chuck Rice & Codfish Gravy',
+    imagePath: 'assets/images/ChuckRice_&_Gravy.jpg',
+    ingredients: [
+      'Garlic powder, season to taste\n',
+      'Onion powder, season to taste\n',
+      'Avocado oil, for frying\n',
+      '2 Red bell peppers, chopped\n',
+      '1 Onion, chopped\n',
+      '1 habernero pepper, chopped\n',
+      '1 can of tomato paste\n',
+      'However many cups of basmatis or jasmine rice you want'
+      '3 Knoor seasoning cubes\n',
+      '1 pack of jute leaf\n',
+      'Fish powder, season to taste\n',
+      '6 cloves of garlic, 2 tablesppons\n',
+      '1 tablespoon of butter unsalted\n'
+      '1 pack of salted codfish',
+    ],
+    instructions: 'Step 1: Bring a bowl of water to a boil and boil your codfish for 11-13 minutes.\n Step 2: While your codfish is boiling cut up all your vegetebales and set them to the side.\n Step 3: Get your rice, wash it, drain it, add a dash of salt to it, and boil it.\n Step 4: Your codfish should be down by this time and since it is salted you are going to waste your water, fill it up with cold water, pull apart the codfish, waste that water, and wash it one last time for good measure. Once you have done that set it to the side.\n Step 5: For your jute leaf if not minced arealdy go ahead and mince it. Bring 2-3 cups of boiling water to a boil. Then add your jute leaf, season with salt and pepper, bring it to a boil. Then add your 6 cloves of garlic and butter and boil for 2 minutes.\n Step 6: After your jute leaf is done take it and add it to your finished rice. Then set it to the side.\n Step 7: Add your avocado oil to a wok or pan of choice. Wait until the oil is hot to add your vegetables. Once added cook your vegetables down until the onions start to sweat add your tomato paste. Then season with garlic powder, onion powder, 3 knoor seasoning cubes, fish powder and black pepper.\n Step 8: Cook the paste until it starts to deepen in color. Then add your dry codfish, can still be wet just no need for water. Fry it unitl the codfish becomes slightly charred in look.\n Step 9: After you have fried your codfish take it off the stove and put it in the oven at 400 F for 15-20 minutes. After that you have your codfish gravy and chuck rice.',
+  ),
+
+  Recipe(
+    name: 'Indomie Noodles',
+    imagePath: 'assets/images/Indomie.jpg',
+    ingredients: [
+      'As many packs of indomie you want',
+      'Your vegetables of choice',
+      'As many eggs you want',
+      '1/2 cude of maggi seasoning',
+      'Avocado oil'
+    ],
+    instructions: 'Step 1: Boil your eggs however you want them.\n Step 2: While your eggs are boiling boil your indomie noodles\n Step 3: Once your noodles have finished boiling drain them and set them to the side. You should do the same with your eggs.\n Step 4: Put a wok or pan of your choice on the stove with avocado oil and bring it to a high temp before you add your noodles and egg.\n Step 5: Add the seasoning to your noodles with the maggi involved and mix it up before adding it to the wok.\n Step 6: Once you have finished all the mixing necessary for the noodles add it to the wok and stir fry to your desired crsipness. You can add the eggs at any point before, after, or during. Once you have reached your desired crispness pull it from the fire and enjoy your noodles.',
+  ),
+
+];
